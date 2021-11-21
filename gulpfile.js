1fe2f882f184db5a1b533bb06d2fdbc283bd09c2
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-11-21 16:50:53 +0100

    feat: versió bàsica de gulp per producció

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,33 @@
+/**
+ * Com emprar aquest fitxer:
+ *  - Comprova que les rutes estan correctes en els llocs
+ * marcats amb // CHECK
+ */
+
 const gulp = require("gulp");
 const browserify = require("browserify");
 const babelify = require("babelify");
-
 const source = require("vinyl-source-stream");
 const buffer = require("vinyl-buffer");
 const uglify = require("gulp-uglify");
-
 const htmlmin = require("gulp-htmlmin");
-
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
-
 const imagemin = require("gulp-imagemin");
-
 const del = require("del");
 
-const sass = require("gulp-sass");
-const browserSync = require("browser-sync").create();
 
 // Source and build directories
 const paths = {
+  // CHECK
   source: "./src",
   build: "./dist",
 };
 
 
 function javascriptBuildMain() {
+  // CHECK - Posa aqui el nom del principal fitxer javascript
+  
   let fileName = 'main.js'
 
   return (
@@ -43,31 +45,11 @@ function javascriptBuildMain() {
   );
 }
 
-function javascriptBuildSecondary() {
-  let fileName = 'anotherJsFile.js'
-  
-  return (
-    browserify({
-      entries: [`${paths.source}/js/${fileName}`],
-      transform: [babelify.configure({ presets: ["@babel/preset-env"] })],
-    })
-      .bundle()
-      .pipe(source(`js/${fileName}`))
-      // Turn it into a buffer!
-      .pipe(buffer())
-      // And uglify
-      .pipe(uglify())
-      .pipe(gulp.dest(`${paths.build}`))
-  );
-}
-
-/**
- * En cas de que emprem més fitxers 
- */
 
 // Write our html task in a saperate function
 function htmlBuild() {
   return gulp
+  // CHECK - comprova la ruta dels fitxers HTML
     .src(`${paths.source}/*.html`)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(paths.build));
@@ -75,7 +57,8 @@ function htmlBuild() {
 
 // Processa el CSS
 function cssBuild() {
-  return gulp
+  return gulp  
+  // CHECK - comprova la ruta dels fitxers CSS
     .src(`${paths.source}/**/css/*.css`)
     .pipe(postcss([cssnano()]))
     .pipe(gulp.dest(`${paths.build}`));
@@ -84,53 +67,22 @@ function cssBuild() {
 // Comprimeix les imatges
 function imgSquash() {
   return gulp
+    // CHECK - comprova la ruta dels fitxers d'imatge
     .src(`${paths.source}/assets/images/*`)
     .pipe(imagemin())
     .pipe(gulp.dest(`${paths.build}/assets/images`));
 }
 
-// Trasllada les fonts de directori
-function fontsToDist() {
-  return gulp.src(`${paths.source}/assets/fonts/*`).pipe(gulp.dest(`${paths.build}/assets/fonts`));
-}
 
 function cleanup() {
-  // Simply execute del with the build folder path
+  // Neteja la carpeta dist
   return del([paths.build]);
 }
 
-//compile scss into css
-function style() {
-  return gulp
-    .src("src/scss/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
-}
 
-// We have to change our exposed task, these functions can be ran in parallel as they do not depend on eachother.
-// If your functions should run synchronously use gulp.series()
-
-function watch() {
-  browserSync.init({
-    server: {
-      baseDir: "./src",
-      index: "/index.html",
-    },
-  });
-  gulp.watch("src/scss/*.scss", style);
-  gulp.watch("src/*.html").on("change", browserSync.reload);
-  gulp.watch("src/js/**/*.js").on("change", browserSync.reload);
-  //gulp.watch("src/images/raw*", imgSquash);
-}
-
-// We have to run the cleanup task first, after which we can run the build tasks
 exports.build = gulp.series(
   cleanup,
   htmlBuild,
   imgSquash,
-  fontsToDist,
-  gulp.parallel(javascriptBuildMain, javascriptBuildSecondary, cssBuild, imgSquash)
+  gulp.parallel(javascriptBuildMain, cssBuild, imgSquash)
 );
-
-exports.watch = watch;
--- src/js/anotherJsFile.js
+++
@@ -1,6 +0,0 @@
-var res=0;
-
-function doNothing() {
-    console.log("res")
-    //Aquesta funcio no fa res
-}
